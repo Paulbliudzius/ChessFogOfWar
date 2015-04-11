30a4a4121c5c7ad01a9e521496811deb790eb874
Paul Bliudzius <bliudzpp@rose-hulman.edu>
2015-04-11 07:54:16 -0400

    Chess Folder, containing the unity files

---
+++ Chess/Assets/ChessPlayer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChessPlayer : MonoBehaviour {
+	int GAMESIZE = 8;
+	private GameObject[] pieces;
+
+	// Use this for initialization
+	void Start () {
+		pieces = new GameObject[16];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public GameObject[] getPieces(){
+		return pieces;
+	}
+	public bool addPiece(GameObject piece){
+		for (int i=0; i<16; i++) {
+			if(pieces[i]==null){
+				pieces[i]=piece;
+				//TODO: Update everything, though maybe should be somewhere else.
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public bool removePiece(int x, int z){
+		for (int i=0; i<16; i++) {
+			if(pieces[i]!=null){
+				if(pieces[i].transform.position.x==x&&pieces[i].transform.position.z==z){
+					Destroy (pieces[i]);
+					pieces[i]=null;
+					//TODO: Update everything, though maybe should be somewhere else.
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public bool[][] piecePositions(){
+		return new bool[][]{
+			new bool[GAMESIZE],
+			new bool[GAMESIZE],
+			new bool[GAMESIZE],
+			new bool[GAMESIZE],
+			new bool[GAMESIZE],
+			new bool[GAMESIZE],
+			new bool[GAMESIZE],
+			new bool[GAMESIZE]
+		};
+	}
+}
---
+++ Chess/Assets/GameBehavior.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBehavior : MonoBehaviour {
+
+	public int GAMESIZE = 8;
+
+	public Transform lights;
+
+	public GameObject white;
+
+	public GameObject opposite;
+
+	public GameObject pawn;
+
+	//public GameObject[][] gamePieces;
+	
+	private GameObject[][] gameSpaces;
+
+	private Transform[][] gameLights;
+
+	private ChessPlayer owner; 
+
+	private ChessPlayer opponent;
+	
+	// Use this for initialization
+	void Start () {
+		owner = GetComponent<ChessPlayer>();
+		gameSpaces=new GameObject[][]{
+			new GameObject[GAMESIZE],
+			new GameObject[GAMESIZE],
+			new GameObject[GAMESIZE],
+			new GameObject[GAMESIZE],
+			new GameObject[GAMESIZE],
+			new GameObject[GAMESIZE],
+			new GameObject[GAMESIZE],
+			new GameObject[GAMESIZE]
+		};
+		gameLights=new Transform[][]{
+			new Transform[GAMESIZE],
+			new Transform[GAMESIZE],
+			new Transform[GAMESIZE],
+			new Transform[GAMESIZE],
+			new Transform[GAMESIZE],
+			new Transform[GAMESIZE],
+			new Transform[GAMESIZE],
+			new Transform[GAMESIZE]
+		};
+		bool odd = true;
+		for (int z=0; z<GAMESIZE; z++) {
+			for (int x=0; x<GAMESIZE; x++) {
+				if(z<2||z>=GAMESIZE-2){
+					Instantiate (pawn,new Vector3(x,(float)0.3,z),Quaternion.identity);
+				}
+				gameLights[x][z] = Instantiate (lights,new Vector3(x,0,z),Quaternion.identity) as Transform;
+				if(odd){
+					gameSpaces[x][z] = Instantiate (opposite,new Vector3(x,0,z),Quaternion.identity) as GameObject;
+				}else{
+					gameSpaces[x][z] = Instantiate (white,new Vector3(x,0,z),Quaternion.identity) as GameObject;
+				}
+				odd=!odd;
+			}
+			odd=!odd;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonDown(0)){
+			Debug.Log ("Click");
+		}
+	}
+}
